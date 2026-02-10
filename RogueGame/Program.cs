11886d0c1e411c54fd2d4cd66a6cd106b6df9af2
G00dS0ul <iseoluwaolowogoke@student.oauife.edu.ng>
2026-02-10 22:41:35 +0100

    Create the solution and implement the mutilple console

--- RogueGame/Program.cs
+++ RogueGame/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RLNET;
+
+namespace RogueGame
+{
+    public class Game
+    {
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+        private static RLRootConsole? _rootConsole;
+
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 50;
+        private static RLConsole _mapConsole;
+
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
+
+        public static void Main()
+        {
+            var fontFileName = "terminal8x8.png";
+
+            var consoleTitle = "G00DS0ULRogueGame";
+
+                _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+                _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+                _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+                _statConsole = new RLConsole(_statWidth, _statHeight);
+                _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
+            _rootConsole.Update += OnRootConsoleUpdate;
+
+            _rootConsole.Render += OnRootConsoleRender;
+
+            _rootConsole.Run();
+        }
+
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _mapConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Black);
+            _mapConsole.Print(1, 1, "Map", RLColor.White);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "Message", RLColor.White);
+            
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
+            _statConsole.Print(1, 1, "Stats", RLColor.White);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
+            _inventoryConsole.Print(1, 1, "Inventory", RLColor.White);
+
+            _rootConsole?.Print(10, 10, "It Worked!", RLColor.White);
+        }
+
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+
+            _rootConsole?.Draw();
+        }
+    }
+}
+
